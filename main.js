092aa003fead4bb8d9e66bd4ad1ff1fc3c49cc3d
Miguel Tafur <migueltafurr@gmail.com>
2024-07-19 10:38:36 -0500

    Corrección url api

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async (event) =>
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch("http://localhost:4000/api/login", {
+        const response = await fetch("https://back-end-cyan-seven.vercel.app/api/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ correo, password }),
